34833e6be7b03685719909612d687d96b34042a2
Naseem Ahmad <naseemahmad1998@gmail.com>
2025-11-03 11:35:36 +0530

    Created the controller & fetch data from db and display on view

--- MagazineApp/Controllers/CategoryController.cs
+++ MagazineApp/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagazineApp.Data;
+using MagazineApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagazineApp.Controllers
 {
     public class CategoryController : Controller
     {
+        public readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+                _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList=_db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
